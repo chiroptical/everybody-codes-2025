6ef0ff0ede042fd9180552736210f5756873c801
Barry Moore <chiroptical@gmail.com>
2025-11-11 11:50:50 -0500

    Quest 6: clean-up part-1, finish part-2, start part-3

--- quest-6-test.rkt
+++ quest-6-test.rkt
@@ -5,4 +5,14 @@
 
 ; AaAaa 1 2 2
 (check-equal? (part-1 "inputs/quest-6-test.txt") 5 "Part 1 test input")
-(check-equal? (part-1 "inputs/quest-6.txt") 172 "Part 1 test input")
+(check-equal? (part-1 "inputs/quest-6.txt") 172 "Part 1 input")
+
+(check-equal? (part-2 "inputs/quest-6-test.txt") 11 "Part 2 test input")
+(check-equal? (part-2 "inputs/quest-6-part-2.txt") 3920 "Part 2 input")
+
+(check-equal? (sized-append 2 (vector 1) 2) (vector 1 2) "sized append works")
+(check-equal? (sized-append 2 (vector 1 2) 3)
+              (vector 2 3)
+              "sized append works, hits limit")
+
+(check-equal? (part-3 "inputs/quest-6-test.txt") '() "...")
--- quest-6.rkt
+++ quest-6.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
-(provide part-1)
+(provide part-1
+         part-2
+         sized
+         sized-append
+         part-3)
 
 (struct state (mentors pairs))
 
@@ -10,16 +14,79 @@
           (foldl
            (lambda (c acc)
              (let ([current-mentors (state-mentors acc)]
-                   [current-pairs (state-pairs acc)]
-                   [is-sword-fighter (and (char-lower-case? c) (char=? c #\a))]
-                   [upper (char-upcase c)])
-               (if is-sword-fighter
-                   (let ([add-pairs (hash-ref current-mentors upper 0)])
-                     (state current-mentors (+ current-pairs add-pairs)))
-                   ; TODO: not a clean fold, should use immutable hash map
-                   (begin
-                     (hash-update! current-mentors c (lambda (x) (+ x 1)) 0)
-                     (state current-mentors current-pairs)))))
-           (state (make-hash) 0)
+                   [current-pairs (state-pairs acc)])
+               (match c
+                 [x
+                  #:when (char=? x #\A)
+                  (state (hash-update current-mentors x (lambda (y) (+ y 1)) 0)
+                         current-pairs)]
+                 [x
+                  #:when (char=? x #\a)
+                  (let ([add-pairs (hash-ref current-mentors #\A 0)])
+                    (state current-mentors (+ current-pairs add-pairs)))]
+                 [_ acc])))
+           (state (make-immutable-hash) 0)
            (string->list mentor-list))])
     (state-pairs final)))
+
+(define (part-2 file)
+  (let* ([mentor-list (call-with-input-file file (lambda (in) (read-line in)))]
+         [final
+          (foldl
+           (lambda (c acc)
+             (let ([current-mentors (state-mentors acc)]
+                   [current-pairs (state-pairs acc)])
+               (match c
+                 [x
+                  #:when (char-upper-case? x)
+                  (state (hash-update current-mentors x (lambda (y) (+ y 1)) 0)
+                         current-pairs)]
+                 [x
+                  #:when (char-lower-case? x)
+                  (let* ([upper (char-upcase x)]
+                         [add-pairs (hash-ref current-mentors upper 0)])
+                    (state current-mentors (+ current-pairs add-pairs)))])))
+           (state (make-immutable-hash) 0)
+           (string->list mentor-list))])
+    (state-pairs final)))
+
+; left and right can contain up to 1000 people
+; focus contains the current character
+(struct zipper (left focus right))
+
+(require racket/generator)
+
+; TODO: part-3
+; https://docs.racket-lang.org/reference/treelist.html
+;
+; A.................
+; ^^               ^
+;  |               | 1000
+;
+; Initial zipper, generator grabs the next character to make,
+;
+;   .a................
+;   ^ ^              ^
+; 1 | |              | 1000
+;
+; - file forms an infinite character generator
+; - read 1001 characters from the generator, forms initial treelist
+; - read subsequent character, modify tree list
+; - do tent calculation for left and right tree
+; - add to pairs
+; - note: the right side of the zipper needs to contract past num-iterations
+(define (part-3 file)
+  (let* ([mentor-chars (call-with-input-file file (lambda (in) (read-line in)))]
+         [num-chars (length mentor-chars)]
+         [num-iterations (* 1000 num-chars)]
+         [mentor-seq (in-string mentor-chars)]
+         [gen (sequence->repeated-generator mentor-seq)]
+         ; TODO: get 1001 characters, build zipper
+         [initial-state 0])
+    (foldl (lambda (idx acc)
+             (let (; TODO: we only pull from `gen` if we haven't reached the end
+                   [should-gen #f]
+                   [c (gen)])
+               acc))
+           initial-state
+           (range 0 num-iterations))))
