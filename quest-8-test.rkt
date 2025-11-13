a53271b40cbfbdbb7975786c73b71b0d52afa8cd
chiroptical <chiroptical@gmail.com>
2025-11-12 21:09:30 -0500

    Finish Quest 8, Part 2

--- quest-8-test.rkt
+++ quest-8-test.rkt
@@ -5,3 +5,21 @@
 
 (check-equal? (part-1 "inputs/quest-8-test.txt" 8) 4 "part 1 test")
 (check-equal? (part-1 "inputs/quest-8.txt" 32) 58 "part 1")
+
+(check-equal? (overlaps? (vec 1 5) (vec 2 6)) #t "overlaps work")
+(check-equal? (overlaps? (vec 1 5) (vec 2 5)) #f "overlaps work")
+(check-equal? (overlaps? (vec 1 5) (vec 5 8)) #f "overlaps work")
+(check-equal? (overlaps? (vec 1 5) (vec 4 8)) #t "overlaps work")
+(check-equal? (overlaps? (vec 1 5) (vec 6 8)) #f "overlaps work")
+
+(check-equal? (overlaps? (vec 2 6) (vec 1 4)) #t "overlaps work")
+(check-equal? (overlaps? (vec 2 5) (vec 1 4)) #t "overlaps work")
+
+(check-equal? (overlaps? (vec 1 5) (vec 3 7)) #t "overlaps work")
+(check-equal? (overlaps? (vec 1 4) (vec 3 7)) #t "overlaps work")
+(check-equal? (overlaps? (vec 2 5) (vec 3 7)) #t "overlaps work")
+(check-equal? (overlaps? (vec 2 6) (vec 3 7)) #t "overlaps work")
+(check-equal? (overlaps? (vec 6 8) (vec 3 7)) #t "overlaps work")
+
+(check-equal? (part-2 "inputs/quest-8-test-2.txt") 21 "part 2 test")
+(check-equal? (part-2 "inputs/quest-8-2.txt") 2925920 "part 2")
--- quest-8.rkt
+++ quest-8.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
-(provide part-1)
+(provide part-1
+         part-2
+         overlaps?
+         vec)
 
 (require "util.rkt")
 
@@ -24,3 +27,38 @@
                     acc)]))
            0
            (windows 2 1 notes))))
+
+(struct vec (lower upper) #:transparent)
+
+(struct state (prev knots) #:transparent)
+
+(define (overlaps? v1 v2)
+  (let ([x (vec-lower v1)]
+        [y (vec-upper v1)]
+        [a (vec-lower v2)]
+        [b (vec-upper v2)])
+    (or (and (> a x) (< a y) (> b y)) (and (> x a) (< x b) (> y b)))))
+
+(define (next-state vec s)
+  (let* ([prev (state-prev s)]
+         [knots (state-knots s)]
+         [new-knots (foldl (lambda (p-vec acc)
+                             (if (overlaps? p-vec vec)
+                                 (+ acc 1)
+                                 acc))
+                           knots
+                           prev)])
+    (state (append prev (list vec)) new-knots)))
+
+(define (make-vecs x)
+  (match x
+    [(list y z)
+     (if (> y z)
+         (vec z y)
+         (vec y z))]))
+
+(define (part-2 file)
+  (let* ([notes (read-notes file)]
+         [vecs (map make-vecs (windows 2 1 notes))]
+         [new-state (foldl next-state (state (list) 0) vecs)])
+    (state-knots new-state)))
