d5726fc3c872ea0fa4d4798fe3e887c49306e977
chiroptical <chiroptical@gmail.com>
2025-11-19 21:34:15 -0500

    Finish Quest 10, Part 2

--- quest-9-test.rkt
+++ quest-9-test.rkt
@@ -5,3 +5,5 @@
 
 (check-equal? (part-1 "inputs/quest-9-test.txt") 414 "part 1 test")
 (check-equal? (part-1 "inputs/quest-9.txt") 7120 "part 1")
+
+(check-equal? (part-1 "inputs/quest-9-2.txt") 317641 "part 2")
