01e6702d3d95f7b82cab0b1a972f6070047ee7cd
chiroptical <chiroptical@gmail.com>
2025-11-20 21:39:23 -0500

    Got correct answer for Quest 10, Part 1. Need to fix code.

--- quest-10-test.rkt
+++ quest-10-test.rkt
@@ -5,5 +5,4 @@
 
 (check-equal? (part-1 "inputs/quest-10-test.txt" 3) 26 "Part 1 test input")
 
-; TODO: This is incorrect
-(check-equal? (part-1 "inputs/quest-10.txt" 4) 140 "Part 1 input")
+(check-equal? (part-1 "inputs/quest-10.txt" 4) 145 "Part 1 input")
