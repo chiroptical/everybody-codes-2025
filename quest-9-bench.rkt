64e973fbf5ed37161d425bbf5f1c8112e2a62306
chiroptical <chiroptical@gmail.com>
2025-11-21 08:50:51 -0500

    Get benchmark for Quest 9 to run

--- quest-9-bench.rkt
+++ quest-9-bench.rkt
@@ -1,16 +1,24 @@
 #lang racket
 
 (require benchmark)
+(require plot)
 (require "quest-9.rkt")
 
 (time (part-1-serial "inputs/quest-9-2.txt"))
 (time (part-1 "inputs/quest-9-2.txt"))
 
-(run-benchmarks (list 'serial 'parallel)
-                (list (list "inputs/quest-9-2.txt"))
-                (lambda (op input)
-                  (let ([fn (match op
-                              ['serial (lambda (i) (part-1-serial i))]
-                              ['parallel (lambda (i) (part-1 i))])])
-                    (fn input)))
-                #:num-trials 30)
+(define benchmark-results
+  (run-benchmarks (list 'serial 'parallel)
+                  (list (list "inputs/quest-9-2.txt"))
+                  (lambda (op input)
+                    (let ([fn (match op
+                                ['serial (lambda (i) (part-1-serial i))]
+                                ['parallel (lambda (i) (part-1 i))])])
+                      (fn input)))
+                  #:num-trials 30
+                  #:extract-time 'delta-time))
+
+(define renderer
+  (render-benchmark-alts (list "inputs/quest-9-2.txt") benchmark-results))
+
+(plot renderer #:out-file "quest-9.png")
